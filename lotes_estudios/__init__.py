385548c46bd5314406b2bef305636bac6921b5a3
miguelmontcerv <mmonteros1pn@gmail.com>
2023-11-12 22:09:10 -0600

    Traducciones

--- lotes_estudios/__init__.py
+++ lotes_estudios/__init__.py
@@ -1,5 +1,6 @@
 import azure.functions as func
 import requests
+import json
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
     # Obtener el parámetro 'estudio' de la solicitud
@@ -18,8 +19,21 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
 
         # Verificar si la solicitud fue exitosa (código de estado 200)
         if respuesta.status_code == 200:
-            # Devolver la imagen y establecer el tipo de contenido en la respuesta HTTP
-            return func.HttpResponse(respuesta.content, mimetype="image/png")
+             # Obtener la descripción (puedes personalizar esto según tus necesidades)
+            descripcion = f"Descripción para {estudio}"
+
+            # Devolver la imagen, su id y la descripción en un formato JSON
+            respuesta_json = {
+                "imagen": respuesta.content.decode("latin-1"),  # Decodificar la imagen y convertirla a cadena
+                "id": estudio,
+                "descripcion": descripcion
+            }
+
+            # Convertir el diccionario a una cadena JSON
+            respuesta_json_str = json.dumps(respuesta_json)
+
+            # Devolver la cadena JSON y establecer el tipo de contenido en la respuesta HTTP
+            return func.HttpResponse(respuesta_json_str, mimetype="application/json")
         else:
             return func.HttpResponse(f"Error al obtener la imagen. Código de estado: {respuesta.status_code}", status_code=500)
 
