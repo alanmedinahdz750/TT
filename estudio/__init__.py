6ebf8aeb92b85fbf49764536c2610de65564333f
alanmedinahdz750 <alanmedinahdz75@gmail.com>
2023-11-30 19:18:06 -0600

    estudios->estudio

--- estudio/__init__.py
+++ estudio/__init__.py
@@ -125,7 +125,38 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
 
             # Enviar la respuesta HTTP con el id
             return func.HttpResponse("Estudio con id: {}, Actualizado".format(str(query)), status_code=200)
+        
+     #++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++  D E L E T E  +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+        elif req.method == 'DELETE':
+
+            # Obtener los datos del cuerpo de la solicitud HTTP en formato JSON
+            datos = req.get_json()
+            idEstudio = req.params.get('idEstudio')
+
+            if idEstudio is None: return func.HttpResponse('Error: Se requiere el id del estudio.', status_code=400)
+
+            # Conexión a la base de datos
+            cnx = mysql.connector.connect(user="dicomate", password="trabajoterminal1$", host="db-dicomate.mysql.database.azure.com", port=3306, database="TT", ssl_disabled=False)
+            cursor = cnx.cursor()
+
+            try:
+                # Verificar que el estudio exista y no sea del mismo usuario
+                query = "DELETE FROM estudios WHERE id = %s"
+                values = (idEstudio,)
+                cursor.execute(query, values)
+
+            except Exception as e:
+                cnx.rollback()
+                return func.HttpResponse('Error al borrar: {}'.format(str(e)), status_code=500)
             
+            finally:
+                cursor.close()
+                cnx.close()
+            
+            # Enviar la respuesta HTTP con el id
+            return func.HttpResponse("Estudio con id: {}, Borrado".format(str(idEstudio)), status_code=200)
+                   
     #++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++  O T R O S  +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
         else: 
