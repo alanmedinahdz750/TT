3c0c49b1caea733f75e6a1879c42c36d278a2187
alanmedinahdz750 <alanmedinahdz75@gmail.com>
2023-12-01 21:55:48 -0600

    borrar_estudio

---
+++ borrar_estudio/__init__.py
@@ -0,0 +1,35 @@
+import logging
+import mysql.connector
+import azure.functions as func
+
+
+def main(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info('Python HTTP trigger function processed a request.')
+
+    if req.method == 'DELETE':
+
+        # Obtener los datos del cuerpo de la solicitud HTTP en formato JSON
+        idEstudio = req.params.get('idEstudio')
+        if idEstudio is None: return func.HttpResponse('Error: Se requiere el id del estudio.', status_code=400)
+
+        # Conexión a la base de datos
+        cnx = mysql.connector.connect(user="dicomate", password="trabajoterminal1$", host="db-dicomate.mysql.database.azure.com", port=3306, database="TT", ssl_disabled=False)
+        cursor = cnx.cursor()
+
+        try:
+            # Verificar que el estudio exista y no sea del mismo usuario
+            query = "DELETE FROM estudios WHERE id = %s"
+            values = (idEstudio,)
+            cursor.execute(query, values)
+
+        except Exception as e:
+            cnx.rollback()
+            return func.HttpResponse('Error al borrar: {}'.format(str(e)), status_code=500)
+        
+        finally:
+            cursor.close()
+            cnx.close()
+        
+        # Enviar la respuesta HTTP con el id
+        return func.HttpResponse("Estudio con id: {}, Borrado".format(str(idEstudio)), status_code=200)
+        
