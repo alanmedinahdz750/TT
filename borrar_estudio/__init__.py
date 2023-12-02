56931ba148f2ef183fd58925934ba74a91c6334b
alanmedinahdz750 <alanmedinahdz75@gmail.com>
2023-12-01 21:57:59 -0600

    POST borrar_estudio

--- borrar_estudio/__init__.py
+++ borrar_estudio/__init__.py
@@ -6,7 +6,7 @@ import azure.functions as func
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
-    if req.method == 'DELETE':
+    if req.method == 'POST':
 
         # Obtener los datos del cuerpo de la solicitud HTTP en formato JSON
         idEstudio = req.params.get('idEstudio')
