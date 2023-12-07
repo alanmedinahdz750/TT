5afe3f80b7e1cb42b7247b7b8a42919ac97f0b8e
alanmedinahdz750 <146052288+alanmedinahdz750@users.noreply.github.com>
2023-12-07 15:00:30 -0600

    Correo

--- estudio/__init__.py
+++ estudio/__init__.py
@@ -155,6 +155,41 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
             # Enviar la respuesta HTTP con el id
             return func.HttpResponse("Estudio con id: {}, Borrado".format(str(idEstudio)), status_code=200)
                    
+    #++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++  C O R R E O  +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+        
+        elif req.method == 'CORREO': 
+            
+            # import necessary packages 
+            from email.mime.multipart import MIMEMultipart
+            from email.mime.text import MIMEText
+            import smtplib
+            
+            # create message object instance 
+            msg = MIMEMultipart()
+            message = "Thank you"
+            
+            # setup the parameters of the message 
+            password = "your_password"
+            msg['From'] = "your_address"
+            msg['To'] = "to_address"
+            msg['Subject'] = "Subscription"
+            
+            # add in the message body 
+            msg.attach(MIMEText(message, 'plain'))
+            
+            #create server 
+            server = smtplib.SMTP('smtp.gmail.com: 587')
+            server.starttls()
+            
+            # Login Credentials for sending the mail 
+            server.login(msg['From'], password)
+            
+            # send the message via the server. 
+            server.sendmail(msg['From'], msg['To'], msg.as_string())
+            server.quit()
+            print "successfully sent email to %s:" % (msg['To'])
+
+
     #++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++  O T R O S  +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
         else: 
