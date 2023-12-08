a40a9dfa1e8e88606f07a4bcae6c53fd568bc48e
alanmedinahdz750 <146052288+alanmedinahdz750@users.noreply.github.com>
2023-12-07 20:40:45 -0600

    Update correo

--- correo/__init__.py
+++ correo/__init__.py
@@ -18,7 +18,7 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
             
             idUsuario = req.params.get('idUsuario')
             cCorreo = req.params.get('correo')
-            if idUsuario is None and cCorreo is None: return func.HttpResponse('Error: Se requiere el id del usuario.', status_code=400)
+            if idUsuario is None and cCorreo is None: return func.HttpResponse('Error: Se requiere el id del usuario o el correo.', status_code=400)
 
             # Conexión a la base de datos
             cnx = mysql.connector.connect(user="dicomate", password="trabajoterminal1$", host="db-dicomate.mysql.database.azure.com", port=3306, database="TT", ssl_disabled=False)
@@ -27,20 +27,28 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
             try:
 
                 # Verificar que el estudio exista y no sea del mismo usuario
-                query = "SELECT * FROM usuarios WHERE id = %s"
-                values = (idUsuario,)
+                query = "SELECT contrasena, correo FROM usuarios"
+                if idUsuario is not None:
+                    query += "WHERE id = %s"
+                    values = (idUsuario,)
+                else:
+                    query += "WHERE correo = %s"
+                    values = (cCorreo,)
                 cursor.execute(query, values)
+                cContrasena = cursor.fetchone()
+                cCorreo = cursor.fetchone()
 
-            
+                if cContrasena is None or cCorreo is None: return func.HttpResponse('Error: Usuario o correo no encontrado', status_code=404)
+
                 # create message object instance 
                 msg = MIMEMultipart()
-                message = "Thank you"
+                message = "Se ha solicitado un correo para restablecer la contraseña para su acceso a Dicomate, la contraseña es: " + cContrasena + " Si usted no la ha solicitado, haga caso omiso a este mensaje"
                 
                 # setup the parameters of the message 
-                password = "your_password"
-                msg['From'] = "your_address"
-                msg['To'] = "to_address"
-                msg['Subject'] = "Subscription"
+                password = "trabajoterminal1$"
+                msg['From'] = "dicomate.noreply@gmail.com"
+                msg['To'] = cCorreo
+                msg['Subject'] = "Reestablecimiento de su contraseña de Dicomate"
                 
                 # add in the message body 
                 msg.attach(MIMEText(message, 'plain'))
@@ -55,11 +63,15 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
                 # send the message via the server. 
                 server.sendmail(msg['From'], msg['To'], msg.as_string())
                 server.quit()
-                print("successfully sent email to %s:" % (msg['To']))
+                # print("successfully sent email to %s:" % (msg['To']))
 
             except Exception as e:
                 return func.HttpResponse('Error: {}'.format(str(e)), status_code=500)
 
+            finally:
+                cnx.close()
+
+            return func.HttpResponse('Error: {}'), status_code=200)
     #++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++  O T R O S  +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
         else: 
@@ -70,3 +82,4 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
 
     except Exception as e:
         return func.HttpResponse('Error: {}'.format(str(e)), status_code=500)
+        
