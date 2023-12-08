507346c74d40c01ecf20bab93168644264971634
alanmedinahdz750 <alanmedinahdz75@gmail.com>
2023-12-08 12:26:13 -0600

    Correo con cuenta valida

--- correo/__init__.py
+++ correo/__init__.py
@@ -35,8 +35,13 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
                     query += "WHERE correo = %s"
                     values = (cCorreo,)
                 cursor.execute(query, values)
-                cContrasena = cursor.fetchone()
-                cCorreo = cursor.fetchone()
+
+                                # Pasar los valores a json
+                columns = [column[0] for column in cursor.description]
+                result = dict(zip(columns, cursor.fetchone()))
+
+                cContrasena = result['contrasena']
+                cCorreo = result['correo']
 
                 if cContrasena is None or cCorreo is None: return func.HttpResponse('Error: Usuario o correo no encontrado', status_code=404)
 
@@ -45,7 +50,7 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
                 message = "Se ha solicitado un correo para restablecer la contraseña para su acceso a Dicomate, la contraseña es: " + cContrasena + " Si usted no la ha solicitado, haga caso omiso a este mensaje"
                 
                 # setup the parameters of the message 
-                password = "trabajoterminal1$"
+                password = "shejxlcguetihzag"
                 msg['From'] = "dicomate.noreply@gmail.com"
                 msg['To'] = cCorreo
                 msg['Subject'] = "Reestablecimiento de su contraseña de Dicomate"
@@ -71,7 +76,7 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
             finally:
                 cnx.close()
 
-            return func.HttpResponse('Error: {}'), status_code=200)
+            return func.HttpResponse('Correo enviado a: {}'.format(msg['To']), status_code=200)
     #++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++  O T R O S  +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
         else: 
